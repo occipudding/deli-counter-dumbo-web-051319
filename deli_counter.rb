f2077ba51178702ac9fd5e4e125fe4adeaa855e6
Joshua Suskin <jsuskin85@gmail.com>
2019-05-06 22:14:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,9 @@ def line(names)
     msg += ": "
     names.each{ |name|
     msg += "#{names.index(name) + 1}. #{name}"
+    if names.index(name) < names.size - 1
+      msg += " "
+    end
     }
   end
   puts msg
