591c364bfe2ba199de89944432bc253c31789cef
Joshua Suskin <jsuskin85@gmail.com>
2019-05-06 22:09:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,6 @@
 def line(names)
-  
+  msg = "The line is currently"
+  names.size == 0 ? msg += " empty." : msg += ": " + names.each{ |name|
+    msg += "#{names[names.index(name) + 1]}. #{name}"
+  }
 end
